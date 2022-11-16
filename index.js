dd821bae0983b1992341bd5c0c3862e505c4a945
Felix Kipruto <kimosopfelix@gmail.com>
2022-11-16 19:34:51 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,61 @@
+function add(){
+
+    return a+b;
+}
+
+function subtract(){
+
+    return a-b;
+}
+
+function multiply(){
+    return a*b;
+}
+
+function divide(){
+    return a/b;
+}
+
+function add(a,b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+function increment(a){
+    return a+1;
+}
+function decrement(n){
+    return n-1;
+}
+function makeInt(n){
+    return n;
+
+}
+   
+function makeInt(n){
+    return parseFloat (n,10);
+}
+
+function preserveDecimal(n){
+
+    return parseInt (n);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+
+
+
+
+
